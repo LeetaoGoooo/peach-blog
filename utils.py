5b1b6d878f274dce34146854311f10080bcb0739
Leetao94 <leetao94cn@gmail.com>
2018-11-24 08:39:23 +0800

    fix:浏览量异常

--- utils.py
+++ utils.py
@@ -13,6 +13,14 @@ class Tools:
         @app.template_global('md5')
         def md5(email):
             return hashlib.md5(email.encode('utf-8')).hexdigest()
+        
+        @app.template_global("total_views")
+        def total_views(postviews):
+            total_views = 0
+            for postview in postviews:
+                total_views += postview.views
+            return total_views
+
 
             
 
